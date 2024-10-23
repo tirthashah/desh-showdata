cdb4841b56c7b1098eeff94d51b7a7d904d4875f
Tirtha shah <tirtha.shah@bitnbinary.com>
2024-10-23 14:12:09 +0530

    added

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ options = Options()
 # options.add_argument('--no-sandbox')  # Bypass OS security model
 # options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
 
-print("Using ChromeDriver from:", ChromeDriverManager().install())
-
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 driver.maximize_window()
 
