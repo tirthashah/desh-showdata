897aac02ebf7b9bff0b9e4e36f36e9138bc088be
tirthashah <130537479+tirthashah@users.noreply.github.com>
2024-10-24 11:56:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Create a new instance of the Chrome options
+options = Options()
+
+# Add the headless option
+options.add_argument("--headless")
+
 # Create a new instance of the Chrome driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Navigate to the webpage
 driver.get("http://www.google.com")
